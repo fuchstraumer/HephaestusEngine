d39d4b14dc68e9d4f7ad0a56c9802c12a563e3fe
fuchstraumer <jonesk74@uw.edu>
2017-02-23 15:01:54 -0800

    Outlining inventory stuff.

---
+++ HephaestusEngine/objects/Player.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "stdafx.h"
+#include "common\CommonUtil.h"
+
+/*
+	
+	PLAYER_H
+
+	Defines a player class. 
+
+
+*/
+
+#endif // !PLAYER_H
