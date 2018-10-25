a8d937bf1ed4a3562a8fda05c5220ece927557c7
Keegan <jonesk74@uw.edu>
2018-10-25 09:06:46 -0700

    Update Morton encoding to be templated, use if-constexpr

--- engine/include/util/Morton.hpp
+++ engine/include/util/Morton.hpp
@@ -3,19 +3,39 @@
 #define MORTON_UTILITIES_H
 #include <cstdint>
 #include <immintrin.h>
+#include <type_traits>
 
-constexpr static uint32_t BMI_X_MASK = 0x09249249;
-constexpr static uint32_t BMI_Y_MASK = 0x12492492;
-constexpr static uint32_t BMI_Z_MASK = 0x24924924;
+template<typename T>
+constexpr static T BMI_X_MASK = T(0x9249249249249249);
+template<typename T>
+constexpr static T BMI_Y_MASK = T(0x2492492492492492);
+template<typename T>
+constexpr static T BMI_Z_MASK = T(0x4924924924924924);
 
-static uint32_t morton_encode(const uint32_t x, const uint32_t y, const uint32_t z) {
-    return _pdep_u32(z, BMI_X_MASK) | _pdep_u32(y, BMI_Y_MASK) | _pdep_u32(x, BMI_Z_MASK);
+template<typename T>
+static uint32_t morton_encode(const T x, const T y, const T z) {
+    static_assert(std::is_integral_v<T> && std::is_unsigned_v<T>, "Morton encoding type must be unsigned int type! (u32/u64)");
+    if constexpr (std::is_same_v<T, uint32_t>) {
+        return _pdep_u32(z, BMI_X_MASK<T>) | _pdep_u32(y, BMI_Y_MASK<T>) | _pdep_u32(x, BMI_Z_MASK<T>);
+    }
+    else if constexpr (std::is_same_v<T, uint64_t>) {
+        return _pdep_u64(z, BMI_X_MASK<T>) | _pdep_u64(y, BMI_Y_MASK<T>) | _pdep_u64(x, BMI_Z_MASK<T>);
+    }
 }
 
-static void morton_decode(const uint32_t input_code, uint32_t& result_x, uint32_t& result_y, uint32_t& result_z) {
-    result_x = _pext_u32(input_code, BMI_X_MASK);
-    result_y = _pext_u32(input_code, BMI_Y_MASK);
-    result_z = _pext_u32(input_code, BMI_Z_MASK);
+template<typename T>
+static void morton_decode(const T input_code, T& result_x, T& result_y, T& result_z) {
+    static_assert(std::is_integral_v<T> && std::is_unsigned_v<T>, "Morton decoding type must be unsigned int type! (u32/u64)");
+    if constexpr (std::is_same_v<T, uint32_t>) {
+        result_x = _pext_u32(input_code, BMI_X_MASK<T>);
+        result_y = _pext_u32(input_code, BMI_Y_MASK<T>);
+        result_z = _pext_u32(input_code, BMI_Z_MASK<T>);
+    }
+    else if constexpr (std::is_same_v<T, uint64_t>) {
+        result_x = _pext_u64(input_code, BMI_X_MASK<T>);
+        result_y = _pext_u64(input_code, BMI_Y_MASK<T>);
+        result_z = _pext_u64(input_code, BMI_Z_MASK<T>);
+    }
 }
 
 
